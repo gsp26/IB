4308539bc62e69e3f5103baa13b76851b1241e16
Govind Singh Parihar <g.s.parihar@gmail.com>
2017-04-30 03:44:54 +0530

    Update RotateMatrix.cpp

--- Arrays/RotateMatrix.cpp
+++ Arrays/RotateMatrix.cpp
@@ -1,3 +1,26 @@
+/*You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+You need to do this in place.
+
+Note that if you end up using an additional array, you will only receive partial score.
+
+Example:
+
+If the array is
+
+[
+    [1, 2],
+    [3, 4]
+]
+Then the rotated array becomes:
+
+[
+    [3, 1],
+    [4, 2]
+]*/
+
 void Solution::rotate(vector<vector<int> > &A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.

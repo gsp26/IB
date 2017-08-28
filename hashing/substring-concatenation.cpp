4c04a189dde03d42c7c0826ceca0426b0a9a712c
Govind Singh Parihar <g.s.parihar@gmail.com>
2017-08-28 20:54:25 +0530

    Create substring-concatenation.cpp

---
+++ hashing/substring-concatenation.cpp
@@ -0,0 +1,70 @@
+/*
+You are given a string, S, and a list of words, L, that are all of the same length.
+
+Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+Example :
+
+S: "barfoothefoobarman"
+L: ["foo", "bar"]
+You should return the indices: [0,9].
+(order does not matter).
+*/
+
+vector<int> Solution::findSubstring(string A, const vector<string> &B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    vector<int> sol;
+    
+    int wsize = B[0].size();
+    int lsize = B.size();
+    
+    if(A.size() == 0 || B.size() == 0){
+        return sol;
+    }
+    
+    unordered_map<string, int> myMap;
+    
+    for(int i = 0; i < B.size(); i++){
+        if(myMap.find(B[i]) != myMap.end()){
+            myMap[B[i]]++;
+        }
+        else{
+            myMap[B[i]] = 1;
+        }
+    }
+    
+    int i = 0;
+    
+    while((i + wsize*lsize - 1) < A.size()){
+        unordered_map<string, int> tempMap;
+        int j = 0;
+        while(j < A.size()){
+            string temp = A.substr(i + j*wsize, wsize);
+            if(myMap.find(temp) == myMap.end()){
+                break;
+            }
+            else{
+                if(tempMap.find(temp) == tempMap.end()){
+                    tempMap[temp] = 1;
+                }
+                else{
+                    tempMap[temp]++;
+                }
+                if(tempMap[temp] > myMap[temp]){
+                    break;
+                }
+                j++;
+            }
+            if(j == lsize){
+                sol.push_back(i);
+            }
+        }
+        i++;
+    }
+    
+    return sol;
+}

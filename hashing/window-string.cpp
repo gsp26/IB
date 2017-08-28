a75e0ce12e14b4d7594f049232d2fec0dd2aea13
Govind Singh Parihar <g.s.parihar@gmail.com>
2017-08-28 20:53:02 +0530

    Create window-string.cpp

---
+++ hashing/window-string.cpp
@@ -0,0 +1,69 @@
+/*
+
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in linear time complexity.
+Note that when the count of a character C in T is N, then the count of C in minimum window in S should be at least N.
+
+Example :
+
+S = "ADOBECODEBANC"
+T = "ABC"
+Minimum window is "BANC"
+
+ Note:
+If there is no such window in S that covers all characters in T, return the empty string ''.
+If there are multiple such windows, return the first occurring minimum window ( with minimum start index ).
+
+*/
+
+string Solution::minWindow(string S, string T) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+     auto sizeS = S.length();
+    auto sizeT = T.length();
+    
+    if (sizeS < sizeT)
+        return "";
+        
+    int needToFind[256] = {0};
+    int hasFound[256] = {0};
+    int count = 0;
+    int minLength = INT_MAX;
+    string res = ""; 
+    
+    for (auto i = 0; i<sizeT; ++i)
+        needToFind[T[i]]++;
+    
+    for (int p = 0, q = 0; q<sizeS; ++q)
+    {
+        if (needToFind[S[q]] == 0)
+            continue;
+        
+        hasFound[S[q]]++;
+        
+        if (hasFound[S[q]] <= needToFind[S[q]])
+            ++count;
+        
+        if (count == sizeT)
+        {
+            while (hasFound[S[p]] == 0 || hasFound[S[p]] > needToFind[S[p]])
+            {
+                if (hasFound[S[p]] > needToFind[S[p]])
+                    hasFound[S[p]]--;
+                ++p;
+            }
+            
+            auto currLength = q - p + 1; 
+            if (currLength < minLength)
+            {
+                res.clear();
+                res.append(S.begin()+p, S.begin()+q+1);
+                minLength = currLength;
+            }
+        }
+    }
+    
+    return res;
+}
